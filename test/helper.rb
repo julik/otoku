cad8e2665dce028ad9eaf119d50774c165ccf92e
Julik <me@julik.nl>
2008-11-17 18:58:29 +0100

    More tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,2 +1,54 @@
 require 'test/unit'
 $:.unshift File.dirname(__FILE__) + '/../lib'
+require 'parser'
+
+
+module THelpers
+  def with_cache_dir(dir = File.dirname(__FILE__) + '/temp')
+    old_td = OTOCS.cache_dir
+    begin
+      
+      yield(dir)
+    ensure
+      OTOCS.cache_dir = old_td
+      FileUtils.rm_rf dir
+    end
+  end
+  
+  def assert_string_attribute(attr, value = nil, message = nil)
+    assert_attribute(attr, String, value, message)
+  end
+
+  def assert_integer_attribute(attr, value = nil, message = nil)
+    assert_attribute(attr, Integer, value, message)
+  end
+  
+  def assert_attribute(attr, klass, value = nil, message =  nil)
+    assert_not_nil @node, "@node should be set"
+    
+    message ||= "#{attr} should be a #{klass} attribute on #{@node.class}"
+    assert_respond_to @node, attr, message + " but it does not respond"
+    assert_kind_of klass, @node.send(attr), message
+    
+    assert_equal value, @node.send(attr), ":#{attr} should be equal to #{value}" if value
+  end
+  
+end
+
+module TAccel
+  TEST_CACHE_DIR = File.join File.dirname(__FILE__), "test-parser-cache-#{Time.now.to_i}"
+  Dir.glob(File.dirname(__FILE__) + '/test-parser-cache-*').each do | old_cache |
+    FileUtils.rm_rf(old_cache)
+  end
+  
+  def setup
+    super
+    OTOCS.cache_dir = TEST_CACHE_DIR
+    @archive = OTOCS.read_archive_file(self.class.const_get(:ARCH))
+  end
+  
+  def teardown
+    OTOCS.cache_dir = nil
+    super
+  end
+end
--- test/t_parser.rb
+++ test/t_parser.rb
@@ -3,6 +3,8 @@ require 'parser'
 require 'fileutils'
 
 class ParserTest < Test::Unit::TestCase
+  include THelpers
+  
   def test_read_archive_file
     Dir.glob(File.dirname(__FILE__) + '/samples/*.xml').each do | xml |
       assert_nothing_raised { OTOCS.read_archive_file(xml) }
@@ -22,13 +24,46 @@ class ParserTest < Test::Unit::TestCase
       assert Dir.glob(tdir + '/**/*.parsedarchive').any?, "Cache files should have been created"
     end
   end
+end
+
+class ArchiveTest < Test::Unit::TestCase
+  include THelpers
+  include TAccel
   
-  def with_cache_dir(dir = File.dirname(__FILE__) + '/temp')
-    begin
-      yield(dir)
-    ensure
-      OTOCS.cache_dir = nil
-      FileUtils.rm_rf dir
-    end
+  ARCH = File.dirname(__FILE__) + '/samples/Flame_Archive_Deel215_08Jul15_1036.xml'
+  
+  
+  def test_archive_attributes
+    @node = @archive
+    
+    assert_string_attribute :name, "Flame_Archive_Deel215"
+    assert_string_attribute :machine, "he-flame-01 - flame 2008.SP4"
+    assert_string_attribute :comment
+
+    assert_attribute :creation, DateTime
+    assert_equal 2008, @node.creation.year
+    assert_equal 5, @node.creation.month
+  end
+  
+  def test_archive_to_s
+    assert_equal "Flame_Archive_Deel215 (6 sets)", @archive.to_s
+  end
+  
+  def test_archive_device
+    assert_respond_to @archive, :device
+    assert_kind_of OTOCS::Device, @archive.device
+    @node = @archive.device
+    
+    assert_string_attribute :type, 'VTR'
+    assert_string_attribute :name, 'Betacam PAL'
+    assert_string_attribute :starts_at, '00:01:00:00'
+  end
+  
+  def test_archive_toc
+    assert_respond_to @archive, :toc
+    assert_kind_of OTOCS::TOC, @archive.toc
+    @node = @archive.toc
+    assert @node.respond_to?(:entries)
+    assert_kind_of Enumerable, @node.entries
   end
 end
